14950039ad0a619fbbf6d216e80f14818c614155
Roshan <90826638+roshanx0@users.noreply.github.com>
2026-03-24 11:12:59 +0530

    Create stop_wait.c

---
+++ stop_wait.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main() {
+    int i = 0, j = 0, noframes, x, x2, n;
+
+    printf("Enter number of frames: ");
+    scanf("%d", &noframes);
+
+    n = noframes;
+    srand(time(NULL));
+
+    while (noframes > 0) {
+
+        printf("\nSending frame %d", i);
+
+        x = rand() % 10;
+
+        if (x % 2 == 0) {
+            for (x2 = 1; x2 <= 3; x2++) {
+                printf("\nWaiting for %d seconds", x2);
+                sleep(x2);
+            }
+            printf("\nMissing Acknowledgement for frame %d", i);
+            printf("\nResending frame %d", i);
+            printf("\nACK received for frame %d", i);
+            if (j < n) {
+                printf("\nACK number %d", j);  // ✅ print before increment
+            }
+            j++;
+
+        } else {
+            printf("\nACK received for frame %d", i);
+            if (j < n) {
+                printf("\nACK number %d", j);  // ✅ print before increment
+            }
+            j++;
+        }
+
+        noframes--;
+        i++;
+    }
+
+    printf("\nEnd of stop and wait protocol\n");
+    return 0;
+}
