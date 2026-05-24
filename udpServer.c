75cb42d546dfce5edbcfaf21ed81039c42dce17f
Roshan <90826638+roshanx0@users.noreply.github.com>
2026-05-24 17:51:07 +0530

    Refactor UDP server code and improve comments

--- udpServer.c
+++ udpServer.c
@@ -27,38 +27,53 @@ Stop
 int main() {
     int sfd;
     char buf[2048];
-    struct sockaddr_in serv, cli;
-    socklen_t len = sizeof(cli);
 
+    struct sockaddr_in server, client;
+    socklen_t len = sizeof(client);
+
+    // Create UDP socket
     sfd = socket(AF_INET, SOCK_DGRAM, 0);
-    
-    memset(&serv, 0, sizeof(serv));
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(PORT);
 
-    bind(sfd, (struct sockaddr *)&serv, sizeof(serv));
+    // Fill server address structure
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    // Bind socket
+    bind(sfd, (struct sockaddr *)&server, sizeof(server));
+
     printf("Server listening...\n");
 
-    while (1) { // Keeps server alive for multiple requests
-        // 1. Receive the string directly (includes \0)
-        int n = recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&cli, &len);
-        if (n <= 0) continue;
+    while (1) {
+
+        // Receive string from client
+        int n = recvfrom(sfd, buf, sizeof(buf), 0,
+                         (struct sockaddr *)&client, &len);
+
+        if (n <= 0)
+            continue;
 
         printf("Received: %s\n", buf);
 
-        // 2. Reverse the string in-place
+        // Reverse string
         int i = 0, j = strlen(buf) - 1;
+
         while (i < j) {
             char temp = buf[i];
-            buf[i++] = buf[j];
-            buf[j--] = temp;
+            buf[i] = buf[j];
+            buf[j] = temp;
+
+            i++;
+            j--;
         }
 
-        // 3. Send reversed string back
-        sendto(sfd, buf, n, 0, (struct sockaddr *)&cli, len);
+        // Send reversed string back
+        sendto(sfd, buf, n, 0,
+               (struct sockaddr *)&client, len);
     }
 
     close(sfd);
+
     return 0;
 }
