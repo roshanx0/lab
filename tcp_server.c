4231c7e7ba8e798ae52423f5dcd0e37af76a4dd5
Roshan <90826638+roshanx0@users.noreply.github.com>
2026-05-24 12:20:25 +0530

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -12,51 +12,66 @@ SERVER:                          CLIENT:
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <stdlib.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
-int main() {
-    int serversocket, clientsocket, port;
-    struct sockaddr_in serveraddr, clientaddr;
+int main()
+{
+    int serversocket, clientsocket, port, n;
+    struct sockaddr_in server, client;
     socklen_t len;
-    char message[100];
+
+    char msg[100];
 
     serversocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    bzero((char *)&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    printf("Enter the port number: ");
-    scanf("%d", &port);
-    serveraddr.sin_port = htons(port);
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    server.sin_family = AF_INET;
 
-    bind(serversocket, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    printf("Enter port number: ");
+    scanf("%d", &port);
+
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serversocket, (struct sockaddr *)&server, sizeof(server));
+
     listen(serversocket, 5);
 
-    len = sizeof(clientaddr);
-    printf("\nWaiting for client connection\n");
-    clientsocket = accept(serversocket, (struct sockaddr *)&clientaddr, &len);
-    printf("\nClient connected\n");
+    printf("Waiting for client...\n");
 
-    read(clientsocket, message, sizeof(message));
-    printf("\nReceived from client: %s\n", message);
+    len = sizeof(client);
 
-    // Reverse the string
-    int start = 0, end = strlen(message) - 1;
-    while (start < end) {
-        char temp = message[start];
-        message[start] = message[end];
-        message[end] = temp;
+    clientsocket = accept(serversocket,
+                          (struct sockaddr *)&client,
+                          &len);
+
+    printf("Client connected\n");
+
+    n = read(clientsocket, msg, sizeof(msg) - 1);
+    msg[n] = '\0';
+
+    printf("Message from client: %s\n", msg);
+
+    int start = 0;
+    int end = strlen(msg) - 1;
+
+    while(start < end)
+    {
+        char temp = msg[start];
+        msg[start] = msg[end];
+        msg[end] = temp;
+
         start++;
         end--;
     }
 
-    write(clientsocket, message, sizeof(message));
-    printf("\nReversed message sent to client\n");
+    write(clientsocket, msg, strlen(msg));
+
+    printf("Reversed string sent\n");
 
     close(clientsocket);
     close(serversocket);
 
     return 0;
+}
 }
